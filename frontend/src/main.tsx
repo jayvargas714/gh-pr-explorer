2eb6161216ebd4328532a24e6fe3245c7c54a873
Jay Vargas <jvargas@scalacomputing.com>
2026-04-16 21:21:13 -0400

    Add PR timeline stylesheet with Matrix UI tokens

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,6 +14,7 @@ import './styles/queue.css'
 import './styles/reviews.css'
 import './styles/modals.css'
 import './styles/markdown.css'
+import './styles/timeline.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
